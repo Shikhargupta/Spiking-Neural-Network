c74e6f7b714f69bbe0afadf0ae659ac25223f941
Iris Johnson <ijohnson@niu.edu>
2018-06-13 11:31:11 -0500

    Stage latest changes

--- snn/spike_train.py
+++ snn/spike_train.py
@@ -7,65 +7,60 @@
 
 import numpy as np
 from numpy import interp
-from snn.neuron import neuron
-import random
 from matplotlib import pyplot as plt
-from snn.recep_field import rf
 import imageio
-from snn.rl import rl
-from snn.rl import update
 import math
 from snn.parameters import param as par
+from snn.recep_field import rf
 
 def encode(pot):
 
-	#initializing spike train
-	train = []
+    #initializing spike train
+    train = []
 
-	for l in range(par.pixel_x):
-		for m in range(par.pixel_x):
+    for l in range(pot.shape[0]):
+        for m in range(pot.shape[1]):
 
-			temp = np.zeros([(par.T+1),])
+            temp = np.zeros([(par.T+1),])
 
+            #calculating firing rate proportional to the membrane potential
+            freq = interp(pot[l][m], [-1.069,2.781], [1,20])
+            #print(pot[l][m], freq)
+            # print freq
 
-			#calculating firing rate proportional to the membrane potential
-			freq = interp(pot[l][m], [-1.069,2.781], [1,20])
-			#print(pot[l][m], freq)
-			# print freq
-			if freq<=0:
-				print(error)
+            assert freq > 0
 
-			freq1 = math.ceil(600/freq)
+            freq1 = math.ceil(600/freq)
 
-			#generating spikes according to the firing rate
-			k = freq1
-			if(pot[l][m]>0):
-				while k<(par.T+1):
-					temp[k] = 1
-					k = k + freq1
-			train.append(temp)
-			# print sum(temp)
-	return train
+            #generating spikes according to the firing rate
+            k = freq1
+            if(pot[l][m]>0):
+                while k<(par.T+1):
+                    temp[k] = 1
+                    k = k + freq1
+            train.append(temp)
+            # print sum(temp)
+    return train
 
 if __name__  == '__main__':
-	# m = []
-	# n = []
-	img = imageio.imread("../images/100.png")
+    # m = []
+    # n = []
+    img = imageio.imread("../images/100.png")
 
-	pot = rf(img)
+    pot = rf(img)
 
-	# for i in pot:
-	# 	m.append(max(i))
-	# 	n.append(min(i))
+    # for i in pot:
+    #     m.append(max(i))
+    #     n.append(min(i))
 
-	# print max(m), min(n)
-	train = encode(pot)
-	f = open('train6.txt', 'w')
-	print(np.shape(train))
+    # print max(m), min(n)
+    train = encode(pot)
+    f = open('train6.txt', 'w')
+    print(np.shape(train))
 
-	for j in range(len(train)):
-		for i in range(len(train[j])):
-			f.write(str(int(train[j][i])))
-		f.write('\n')
+    for j in range(len(train)):
+        for i in range(len(train[j])):
+            f.write(str(int(train[j][i])))
+        f.write('\n')
 
-	f.close()
+    f.close()
