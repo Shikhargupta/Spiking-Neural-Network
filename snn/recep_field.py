1c3d0a8cfc1d8a30e1676b8334b566d09d3286c0
Iris Johnson <ijohnson2@niu.edu>
2018-08-06 09:43:39 -0500

    Allow other input image sizes in the filtering and spikegen steps (read the size from the input shape)

--- snn/recep_field.py
+++ snn/recep_field.py
@@ -23,18 +23,18 @@ def rf(inp):
 	 	[	sca3 ,sca2 , sca1 ,sca2 ,sca3],
 	 	[	sca4 ,sca3 , sca2 ,sca3 ,sca4]]
 
-	pot = np.zeros([par.pixel_x,par.pixel_x])
+	pot = np.zeros([inp.shape[0],inp.shape[1]])
 	ran = [-2,-1,0,1,2]
 	ox = 2
 	oy = 2
 
 	#Convolution
-	for i in range(par.pixel_x):
-		for j in range(par.pixel_x):
+	for i in range(inp.shape[0]):
+		for j in range(inp.shape[1]):
 			summ = 0
 			for m in ran:
 				for n in ran:
-					if (i+m)>=0 and (i+m)<=par.pixel_x-1 and (j+n)>=0 and (j+n)<=par.pixel_x-1:
+					if (i+m)>=0 and (i+m)<=inp.shape[0]-1 and (j+n)>=0 and (j+n)<=inp.shape[0]-1:
 						summ = summ + w[ox+m][oy+n] * inp[i+m][j+n]/255
 			pot[i][j] = summ
 	return pot
--- snn/spike_train.py
+++ snn/spike_train.py
@@ -13,6 +13,35 @@ import math
 from snn.parameters import param as par
 from snn.recep_field import rf
 
+def encode2(pixels):
+
+    #initializing spike train
+    train = []
+
+    for l in range(pixels.shape[0]):
+        for m in range(pixels.shape[1]):
+
+            temp = np.zeros([(par.T+1),])
+
+            #calculating firing rate proportional to the membrane potential
+            freq = interp(pixels[l][m], [0, 255], [1,20])
+            #print(pot[l][m], freq)
+            # print freq
+
+            assert freq > 0
+
+            freq1 = math.ceil(600/freq)
+
+            #generating spikes according to the firing rate
+            k = freq1
+            if(pixels[l][m]>0):
+                while k<(par.T+1):
+                    temp[k] = 1
+                    k = k + freq1
+            train.append(temp)
+            # print sum(temp)
+    return train
+
 def encode(pot):
 
     #initializing spike train
@@ -45,7 +74,8 @@ def encode(pot):
 if __name__  == '__main__':
     # m = []
     # n = []
-    img = imageio.imread("../images/100.png")
+    img = imageio.imread("/Users/johnsoni/Downloads/mnist_png/training/5/0.png")
+    #img = imageio.imread("data/training/0.png")
 
     pot = rf(img)
 
@@ -54,6 +84,7 @@ if __name__  == '__main__':
     #     n.append(min(i))
 
     # print max(m), min(n)
+    #train = encode2(img)
     train = encode(pot)
     f = open('train6.txt', 'w')
     print(np.shape(train))
