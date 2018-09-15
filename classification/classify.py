13e3edaf344751015c3f9d3dd09d1a6f6f31b64d
Shikhar Gupta <guptashikhar20@gmail.com>
2018-09-15 22:49:10 +0530

    README for classify.py

--- classification/classify.py
+++ classification/classify.py
@@ -1,3 +1,8 @@
+##################### README ###################################################
+# This file executes the classification algorithm over input testing images.
+# Winner neurons inhibit other neurons by a phenomenon called Lateral inhibition
+# Spike for each output neuron at each time stamp is monitored.
+################################################################################
 import numpy as np
 from neuron import neuron
 import random
